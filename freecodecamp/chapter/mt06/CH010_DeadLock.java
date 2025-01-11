041cb7747fb23476e7ceafc246457b23227e5fa3
antarikshgosain <antariksh.gosain@gmail.com>
2025-01-11 14:46:03 -0500

    detecting deadlocks 2 - getting thread details using mxbean

--- freecodecamp/chapter/mt06/CH010_DeadLock.java
+++ freecodecamp/chapter/mt06/CH010_DeadLock.java
@@ -1,5 +1,7 @@
 package chapter.mt06;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -11,7 +13,7 @@ public class CH010_DeadLock {
         lockA.lock();
         System.out.println("Worker-One acquired lock-A");
         try {
-            Thread.sleep(200);
+            Thread.sleep(2000);
         } catch (InterruptedException e){
             throw new RuntimeException(e);
         }
@@ -24,23 +26,50 @@ public class CH010_DeadLock {
 
     public void workerTwo() {
         lockB.lock();
-        System.out.println("Worker-Two acquired lock-A");
+        System.out.println("Worker-Two acquired lock-B");
         try {
             Thread.sleep(200);
         } catch (InterruptedException e){
             throw new RuntimeException(e);
         }
         lockA.lock();
-        System.out.println("Worker-Two acquired lock-B");
+        System.out.println("Worker-Two acquired lock-A");
         lockB.unlock();
         lockA.unlock();
     }
 
     public static void main(String[] args) {
         CH010_DeadLock demo = new CH010_DeadLock();
+        boolean customDebug = true;
 
         new Thread(demo::workerOne,"Worker-One").start();
         new Thread(demo::workerTwo,"Worker-Two").start();
+
+        new Thread(() -> {
+            ThreadMXBean mxBean = ManagementFactory.getThreadMXBean();
+            while (true) {
+                long[] deadLockedThreadIds  = mxBean.findDeadlockedThreads();
+                long[] allThreadIds         = mxBean.getAllThreadIds();
+                if (customDebug && null != allThreadIds && allThreadIds.length > 0 ) {
+                    for (long tid : allThreadIds) {
+                        System.out.println(String.format("Details about %d \n%s",tid,mxBean.getThreadInfo(tid)));
+                    }
+                }
+                if (null != deadLockedThreadIds && deadLockedThreadIds.length > 0 ) {
+                    System.out.println("Dead Locks Detected!");
+                    for(long tid : deadLockedThreadIds) {
+                        System.out.println(String.format("DeadLock! Details about %d : %s",tid,mxBean.getThreadInfo(tid)));
+                    }
+                    break; // remove this break for continuing infinite while loop
+                }
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
 
     }
 }
